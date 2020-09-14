9b472e0a827aa2ce8d83ba6c8a958e44a458d500
Aman Shekhar <Aman Shekhar>
2020-09-14 13:07:56 +0530

    'java-hacker-rank-solutions'

---
+++ src/25. Java_Big_Decimal.java
@@ -0,0 +1,30 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+       
+        Arrays.sort(s, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1 == null || s2 == null) {
+                    return 0;
+                }
+                BigDecimal bd1 = new BigDecimal(s1);
+                BigDecimal bd2 = new BigDecimal(s2);
+                return bd2.compareTo(bd1);
+            }
+        });
+
+
+
+        //Output
